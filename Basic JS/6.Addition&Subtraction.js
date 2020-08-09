c92f6a9d25a6c7e513019cfdea15102d1f331570
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-09 12:09:11 +0100

    Create 6.Addition&Subtraction.js

---
+++ Basic JS/6.Addition&Subtraction.js
@@ -0,0 +1,17 @@
+// Js can be used for many mathematical operations 
+
+/* Number is a data type in JavaScript which represents numeric data.
+JavaScript uses the + symbol as an addition operator when placed between two numbers.
+
+Example:
+*/
+
+VarB = 5 + 10; // assigned 15
+
+/* We can also subtract one number from another.
+JavaScript uses the - symbol for subtraction.
+
+Example:
+*/
+
+VarA = 12 - 6; // assigned 6
