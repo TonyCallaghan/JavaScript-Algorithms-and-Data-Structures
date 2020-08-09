a74827d516d63c35d044944872e956908146eed9
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-09 11:28:13 +0100

    Create 2.DeclaringVariables.js

---
+++ Basic JS/2.DeclaringVariables.js
@@ -0,0 +1,5 @@
+// Declaring Variables in Js
+  
+// JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+
+var firstName = Tony;
