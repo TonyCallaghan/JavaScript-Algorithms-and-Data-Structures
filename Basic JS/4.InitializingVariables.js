38268e03847f4d33826709e4b60ce42932175e2b
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-09 11:37:19 +0100

    Create 4.InitializingVariables.js

---
+++ Basic JS/4.InitializingVariables.js
@@ -0,0 +1,7 @@
+// Initializing Variables with the Assignment Operator.
+
+// It is common to initialize a variable to an initial value in the same line as it is declared.
+
+var myVar = 5;
+
+// Creates a new variable called myVar and immediately assigns it an initial value of 5.
