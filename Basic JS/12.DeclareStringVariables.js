e70a9144aca08a47bd1c6f167fd912f1bc4fb1fa
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-10 19:48:09 +0100

    Create 12.DeclareStringVariables.js

---
+++ Basic JS/12.DeclareStringVariables.js
@@ -0,0 +1,11 @@
+// Declare String Variables
+
+// Previously we have used the code
+
+var myName = "your name";
+
+// "your name" is called a string literal. 
+// It is a string because it is a series of zero or more characters enclosed in single or double quotes.
+
+var myFirstName = "tony";
+var myLastName = 'call';
