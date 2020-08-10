814332b728f80f9c83646032e1cbdcbb2bad37d4
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-10 19:14:36 +0100

    Create 9.Increment&Decrement.js

---
+++ Basic JS/9.Increment&Decrement.js
@@ -0,0 +1,22 @@
+// Increment a Number with JavaScript
+
+// You can easily increment or add one to a variable with the ++ operator.
+
+i++;
+
+// is the equivalent of
+
+i = i + 1;
+
+//*************
+// Decrement a Number with JavaScript
+
+// You can easily decrement or decrease a variable by one with the -- operator.
+
+i--;
+
+// is the equivalent of
+
+i = i - 1;
+
+// Note:The entire line becomes i++; eliminating the need for the equal sign.
