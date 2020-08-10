4d4c4f8c09792068c9551a79fe87c26fdc0451a5
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-10 18:50:43 +0100

    Create 8.Mult&Division.js

---
+++ Basic JS/8.Mult&Division.js
@@ -0,0 +1,20 @@
+// Multiplication 
+
+/* We can also multiply one number by another.
+JavaScript uses the * symbol for multiplication of two numbers.
+
+Example:
+*/
+
+myVar = 13 * 13; // assigned 169
+
+
+// Division
+
+/* We can also divide one number by another.
+JavaScript uses the / symbol for division.
+
+Example:
+*/
+
+myVar = 16 / 2; // assigned 8
