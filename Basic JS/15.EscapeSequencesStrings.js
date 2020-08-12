affde8388f33471a39d8c338b45161d0baebb81a
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-12 11:51:16 +0100

    Create 15.EscapeSequencesStrings.js

---
+++ Basic JS/15.EscapeSequencesStrings.js
@@ -0,0 +1,26 @@
+// Escape Sequences in Strings
+
+/*
+Quotes are not the only characters that can be escaped inside a string. There are two reasons to use escaping characters:
+
+To allow you to use characters you may not otherwise be able to type out, such as a carriage return.
+To allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+We learned this in the previous challenge.
+
+Code	  Output
+\'	  single quote
+\"	  double quote
+\\	  backslash
+\n	  newline
+\r	  carriage return
+\t	  tab
+\b	  word boundary
+\f	  form feed
+
+*/
+
+var crazyString = "\'hello, This is backslash aka \\\' \n -Tony \t -2020."
+
+//  Returns:
+// 'hello, This is backslash aka \' 
+//  -Tony 	 -2020."
