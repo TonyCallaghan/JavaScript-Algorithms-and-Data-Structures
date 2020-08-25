c331670282296607beec507b4855a2e19073709f
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-25 10:47:11 +0100

    Create 23.NestedArrays.js

---
+++ Basic JS/23.NestedArrays.js
@@ -0,0 +1,12 @@
+// Nest one Array within Another Array
+
+/*
+You can also nest arrays within other arrays, like below:
+
+[["Bulls", 23], ["White Sox", 45]]
+This is also called a multi-dimensional array.
+
+Another Example:
+*/
+
+var myNestedArray = [["C.Ronaldo", 7], ["L.Messi", 10]];
