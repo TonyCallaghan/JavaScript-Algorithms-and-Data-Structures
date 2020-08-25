9c07621713daa0b4aff4c69569e59d1d0bed7427
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-25 11:03:31 +0100

    Create 25.ModifyArrayData.js

---
+++ Basic JS/25.ModifyArrayData.js
@@ -0,0 +1,7 @@
+// Modify Array Data With Indexes
+// Unlike strings, the entries of arrays are mutable and can be changed freely.
+
+// Example:
+
+var ourArray = [50,40,30];
+ourArray[0] = 15; // equals [15,40,30]
