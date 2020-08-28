b4a76c9ed050ca356de35998dadb14a2934587e4
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-08-28 11:40:19 +0100

    Create 31.Ex-Multi-DimensionalArray.js

---
+++ Basic JS/31.Ex-Multi-DimensionalArray.js
@@ -0,0 +1,21 @@
+// Shopping List
+
+/*
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+
+There should be at least 5 sub-arrays in the list.
+*/
+
+var myList = [
+  ["item 1", 5],
+  ["item 2", 5],
+  ["item 3", 5],
+  ["item 4", 5],
+  ["item 5", 5]
+];
+
+// Best practice to write them like this !
