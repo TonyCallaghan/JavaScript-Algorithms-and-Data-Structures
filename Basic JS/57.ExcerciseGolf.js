9a4a105afe0dbd802b57d6bbc948a7ac3618e89a
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2020-09-08 10:19:16 +0100

    Update 57.ExcerciseGolf.js

--- Basic JS/57.ExcerciseGolf.js
+++ Basic JS/57.ExcerciseGolf.js
@@ -18,3 +18,29 @@ par + 2	    "Double Bogey"
 
 par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
 */
+
+var names = ["0","Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[1];
+  } else if (strokes <= par -2) {
+    return names[2];
+  } else if (strokes <= par -1) {
+    return names[3];
+  } else if (strokes == par) {
+    return names[4];
+  } else if (strokes == par +1) {
+    return names[5];
+  } else if (strokes == par +2) {
+    return names[6];
+  } else if (strokes >= par +3) {
+    return names[7];
+  } else {
+    return "Change Me";
+  }
+  
+  // Only change code above this line
+}
+
+golfScore(5, 4);
